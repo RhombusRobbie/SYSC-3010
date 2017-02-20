39b5121ee67f68571f87b8a5b8b7cf5b3a69bc95
huSin <hussainaljabri@cmail.carleton.ca>
2017-02-19 21:22:39 -0800

    Create main.java

---
+++ GUI-DB/main.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class main
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args) throws Exception{
+        DB db = new DB();
+        db.createTable();
+        Login lgn = new Login();
+    }
+}
