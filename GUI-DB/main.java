7a491a93d70e9218405fb432ee78e4f486d6b786
huSin <hussainaljabri@cmail.carleton.ca>
2017-02-25 17:49:54 -0800

    Update main.java

--- GUI-DB/main.java
+++ GUI-DB/main.java
@@ -1,16 +1,18 @@
 
-/**
- * Write a description of class main here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class main
 {
     // instance variables - replace the example below with your own
-    public static void main(String[] args) throws Exception{
-        DB db = new DB();
-        db.createTable();
-        Login lgn = new Login();
+
+    /**
+     * Constructor for objects of class main
+     */
+    public static void main(String args[])
+    {
+           DB db = new DB();
+           db.createTable();
+           mainFrame mf = new mainFrame();
+           Login lgn = new Login(mf);
     }
+
+    
 }
