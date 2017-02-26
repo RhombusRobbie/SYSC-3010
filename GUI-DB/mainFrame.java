5dac986f3a872db8006fc836cb40917c170b8dee
huSin <hussainaljabri@cmail.carleton.ca>
2017-02-25 17:50:14 -0800

    Update mainFrame.java

--- GUI-DB/mainFrame.java
+++ GUI-DB/mainFrame.java
@@ -1,30 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.*;
+import java.io.*;
+import javax.swing.BorderFactory;
 
 public class mainFrame extends JFrame {
+   JTextArea textArea;   
+   JPanel panel1 = new JPanel();
+   JPanel panel2 = new JPanel();
+   
+   mainFrame(){
+      super("Welcome");
+        //String all = "";
+        //try{
+       //     all = new Scanner(new File("C:\\Users\\Husin\\Desktop\\fafa.txt")).useDelimiter("\\A").next();
+       // }catch(Exception e){System.out.println(e);}
+        JLabel label = new JLabel("                                Event history");
+        setLocation(500,280);
+        //panel1.setLayout(new BorderLayout()); 
+        //panel2.setLayout(new BorderLayout());
+        setLayout(new BorderLayout());
+        
+        textArea = new JTextArea(10,20);
+        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        textArea.setEditable(false);
+       // textArea.setText(all);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        
+        //textArea.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK), 
+        //        BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        scrollPane.setPreferredSize(new Dimension(700,400));
+        //panel2.setPreferredSize(new Dimension(100,100));
+        panel1.add(scrollPane);
+        
+        panel2.add(new Button("     Clear History    "));
+        panel2.add(new Button("     Export History    "));
+        //scrollPane.setSize(1000,3000);
+        //setSize(1000,1000);
+        
+        //panel1.setBounds(0,0,250, 500);
+        //panel2.setBounds(252, 0, 248, 500);
+        getContentPane().add(panel1, BorderLayout.CENTER);
+        getContentPane().add(panel2, BorderLayout.EAST);
+        getContentPane().add(label, BorderLayout.NORTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        //setVisible(true);
+    }
 
-public static void main(String[] args) {
-    mainFrame frameTabel = new mainFrame();
+public void update(String str){
+    if(textArea.getText() == ""){
+        textArea.setText(str);
+    }else{
+        textArea.setText(textArea.getText() + "\n" + str);
+    }
 }
-    
-   JLabel welcome = new JLabel("Welcome to a New Frame");
-   JPanel panel = new JPanel();
 
-mainFrame(){
-    super("Welcome");
-    setSize(300,200);
-    setLocation(500,280);
-    panel.setLayout (null); 
-    
-    welcome.setBounds(70,50,150,60);
-    
-    panel.add(welcome);
-    
-    getContentPane().add(panel);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setVisible(true);
-}
 
 }
