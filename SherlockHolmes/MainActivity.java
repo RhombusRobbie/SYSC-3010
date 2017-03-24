b5fccead6618fc4b5469c374fa7e15fc2f9676eb
Shisound <adebola.shittu@yahoo.com>
2017-03-24 10:15:11 -0400

    Update MainActivity.java

--- SherlockHolmes/MainActivity.java
+++ SherlockHolmes/MainActivity.java
@@ -1,34 +1,52 @@
 package com.example.adebola.sherlockholmes;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Username, Password;
+    TextView WrongPassword;
+
+
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WrongPassword = (TextView)findViewById(R.id.textView3);
+        WrongPassword.setVisibility(View.GONE);
     }
 
     /*Called when the User Clicks the Login Button*/
     public void Login(View view){
 
-        //Intent class does something when this method is called. Takes two parameters
-        //the context and the class. MainPage is entered when user clicks button
-        Intent intent = new Intent(this, Mainpage.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
+        Username = (EditText) findViewById(R.id.editText);
+        Password = (EditText) findViewById(R.id.editText2);
 
-        //Add the editText value to the intent. Carry the key and value.
-        //Key is the EXTRA_MESSAGE(use app package name to make it unique)
-        //The key is used to retrieve the text value
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+
+        //allow Login when the Correct Password is Pressed
+        if(Username.getText().toString().equals("sherlock") &&
+                Password.getText().toString().equals("holmes")){
+            //Correct Password
+            Intent intent = new Intent(this, Mainpage.class);
+            startActivity(intent);
+        }else{
+            //Wrong Password
+            WrongPassword.setVisibility(View.VISIBLE);
+            WrongPassword.setBackgroundColor(Color.MAGENTA);
+
+        }
+        
 
     }
 }
