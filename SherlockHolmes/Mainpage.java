823657f465849f79966c3436f5d7ffcaab341286
Shisound <adebola.shittu@yahoo.com>
2017-03-27 00:05:48 -0400

    Update Mainpage.java

--- SherlockHolmes/Mainpage.java
+++ SherlockHolmes/Mainpage.java
@@ -1,37 +1,67 @@
 package com.example.adebola.sherlockholmes;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Mainpage extends AppCompatActivity {
 
+
+    //Tag name for checking the Log
+    private static final String TAG = "MainPage";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+        //Intent for the Service... This runs the ServerService when the App is Logged in
+        Intent ServiceIntent = new Intent(this, ServerService.class);
+        startService(ServiceIntent);
+
+        //Printout Logs of the activity on MainPage
+        Log.i(TAG, "OnCreate");
+
     }
 
 
-     public void History(View view){
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Printout Logs of the activity on MainPage
+        Log.i(TAG, "OnStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Printout Logs of the activity on MainPage
+        Log.i(TAG, "OnPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Printout Logs of the activity on MainPage
+        Log.i(TAG, "OnResume");
+    }
+
+    public void History(View view){
         //Intent class does something when this method is called. Takes two parameters
         //the context and the class. MainPage is entered when user clicks button
 
           Intent intent = new Intent(this, History.class);
           //Start the Next Activity that contains the history
           startActivity(intent);
-
-        /**
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
-        */
     }
 }
