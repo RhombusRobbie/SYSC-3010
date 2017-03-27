d5abce90d1de498c8a616a65205b87edf43fcf95
Shisound <adebola.shittu@yahoo.com>
2017-03-27 00:05:27 -0400

    Update MainActivity.java

--- SherlockHolmes/MainActivity.java
+++ SherlockHolmes/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -46,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             WrongPassword.setBackgroundColor(Color.MAGENTA);
 
         }
-        
+
 
     }
 }
