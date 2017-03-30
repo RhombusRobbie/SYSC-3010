3b82721538679e5152f42b26f215502678bbe492
huSin <hussainaljabri@cmail.carleton.ca>
2017-03-29 22:18:04 -0700

    Update DB.java

--- GUI-DB/DB.java
+++ GUI-DB/DB.java
@@ -1,61 +1,72 @@
  /*
  * table1 -> name,pass
- * table2 -> keyword
+ * table2 -> keyword, emotion
  * history -> event
  */
 import java.sql.*;
  
 public class DB {
-
-   public static void createTable() throws Exception{
+	/*
+	 * Constructor does the following
+	 * 1) creates Table1 used to store name, pass
+	 * 2) creates Table2 used to load in a text file with keyword, emotion.
+	 * 3) creates Table3 used as History, stores every event.
+	 */
+	Connection con;
+	public DB(){
+		con = getConnection();
+		createTable("CREATE TABLE IF NOT EXISTS table1 (name varchar(15), pass varchar(15))");
+		createTable("CREATE TABLE IF NOT EXISTS table2 (keyword varchar(15), emotion varchar(15))");
+		createTable("CREATE TABLE IF NOT EXISTS history (event text)");
+	}
+   public void createTable(String str){
        try{
-           Connection con = getConnection();
-           PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS table1 (name varchar(15), pass varchar(15))");
+           
+           PreparedStatement create = con.prepareStatement(str);
            create.executeUpdate();
-           create = con.prepareStatement("CREATE TABLE IF NOT EXISTS table2 (keyword varchar(15), emotion varchar(15)");
-           create.executeUpdate();
-           create = con.prepareStatement("CREATE TABLE IF NOT EXISTS history (event text");
-           create.executeUpdate();
-        }catch(Exception e){System.out.println("Cannot create tables"); System.exit(1);} 
-       finally{System.out.println("Function complete.");};
+        }catch(Exception e){System.out.println("Cannot create tables"); System.out.println(e);} 
+       finally{System.out.println("Table created.");};
     }
    
-   public static Connection getConnection() throws Exception{
+   
+   public static Connection getConnection(){
        try{
+    	   Connection conn;
+    	   conn = DriverManager.getConnection
+    			   ("jdbc:mysql://localhost/?user=root&password=123456"); 
+    	   conn.createStatement().executeUpdate("CREATE DATABASE db");
            String driver = "com.mysql.jdbc.Driver";
-           String url = "jdbc:mysql://localhost:3306/testdb";
+           String url = "jdbc:mysql://localhost:3306/db";
            String username = "husin";
            String password = "1234";
            Class.forName(driver);
            
-           Connection conn = DriverManager.getConnection(url, username, password);
+           conn = DriverManager.getConnection(url, username, password);
            
            System.out.println("Connected");
            return conn;
         }catch(Exception e){
-            System.out.println("Cannot establish connection to Database"); System.exit(1);
+            System.out.println("Cannot establish connection to Database"); System.out.println(e);
         }
        return null;
     }
 
-  public int contains(String user, String pass){
+  public boolean contains(String user, String pass){
       try{
-          Connection con = getConnection();
           PreparedStatement statement = con.prepareStatement("SELECT name,pass FROM table1");
           ResultSet result = statement.executeQuery();
           while(result.next()){
               if(result.getString("name").equals(user) && result.getString("pass").equals(pass)){
-                  return 1;
+                  return true;
                 }
               
             }
-            return 0;
-        }catch(Exception e){System.out.println("contains(user,pass): Error");System.exit(1);}
-        return 0;
+            return false;
+        }catch(Exception e){System.out.println("contains(user,pass): Error"); System.out.println(e);}
+        return false;
     }
-   public String contains(String keyword){
+   public String getEmotion(String keyword){
       try{
-          Connection con = getConnection();
           PreparedStatement statement = con.prepareStatement("SELECT keyword,emotion FROM table2");
           ResultSet result = statement.executeQuery();
           while(result.next()){
@@ -65,19 +76,26 @@ public class DB {
               
             }
             return null;
-        }catch(Exception e){System.out.println("contains(keyword): Error");System.exit(1);}
+        }catch(Exception e){System.out.println("contains(keyword): Error"); System.out.println(e);}
         return null;
     }
-
   public void setUser(String user, String pass){
       try{
-          Connection con = getConnection();
           PreparedStatement posted = con.prepareStatement("INSERT INTO table1 (name, pass) VALUES ('"+user+"', '"+pass+"')");
           posted.executeUpdate();
         }catch(Exception e){
-        	System.out.println("setUser(user,pass): Error");System.exit(1);
+        	System.out.println("setUser(user,pass): Error"); System.out.println(e);;
         }
         finally{
             System.out.println("Insert Complete.");};
     }
+  public void setEvent(String event){
+	  try{
+		PreparedStatement posted = con.prepareStatement("INSERT INTO history (event) VALUES ('"+event+"')");
+	  	posted.executeUpdate();
+	  }catch(Exception e){ System.out.println("setEvent: Error, "+ e);
+	  }finally{
+		System.out.println("Insert Complete.");
+	  };
+  }
 }
