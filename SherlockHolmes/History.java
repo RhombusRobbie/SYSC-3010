3a199b40d6d3c482a71b6f000bbbf15e5d553cf6
Shisound <adebola.shittu@yahoo.com>
2017-04-05 20:27:05 -0400

    trying to get the Notification working

--- SherlockHolmes/History.java
+++ SherlockHolmes/History.java
@@ -10,24 +10,44 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class History extends AppCompatActivity {
 
     //Notification builder for each notification
     NotificationCompat.Builder notification;
 
+    private static String TAG = "History";
+
     //Notification ID number. so the system knows how to handle each notifications
     private static final int uniqueID = 45612;
+    
+
+    public boolean YESs = TRUE;
 
     public Button call_button;
+
+    String event = " NO Event Yet";
 
     @Override
     //Shows History of the events that had ocurred
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        //Trying to Receive the eventText from Service Activity
+        Log.i(TAG, "Trying to Receive event from service");
+        Intent intent3 = getIntent();
+        Bundle extras = intent3.getExtras();
+
+        event = extras.getString("eventText");
+        extras.putString("eventText", event);
+
 
         call_button = (Button) findViewById(R.id.callbutton);
 
@@ -57,8 +77,9 @@ public class History extends AppCompatActivity {
     }
 
 
-    //Method to build the notification and
-    public void sendNotification(View view){
+
+        //Method to build the notification and
+        public void sendNotification (View view){
 
         //Notification Image
         notification.setSmallIcon(R.drawable.sherlockholmes);
@@ -67,8 +88,12 @@ public class History extends AppCompatActivity {
         notification.setTicker("this is the Ticket");
         //Time of the notification
         notification.setWhen(System.currentTimeMillis());
-        notification.setContentTitle("Title of the notification");
-        notification.setContentText("Text of the notification");
+
+
+
+        notification.setContentTitle("URGENT!!! CHILD IN TROUBLE");
+        notification.setContentText(event);
+
 
         //What happens when the user clicks on the notification. send the user to the mainpage
         Intent intent1 = new Intent(this, History.class);
@@ -79,7 +104,13 @@ public class History extends AppCompatActivity {
         //send out the Notification and issue it(sending it to device)
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         //building it and sending it out
-        nm.notify(uniqueID, notification.build());
-    }
 
+            //Sends out notifcation only when event occurs
+            if (event.length() > 1) {
+                nm.notify(uniqueID, notification.build());
+            }else{
+               YESs = FALSE;
+            }
+    }
 }
+
